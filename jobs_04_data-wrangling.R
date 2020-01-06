71cc5eaf3922856e23448c4da5ce34c595c4e4c8
Marwolaeth <a.pawluczenko@gmail.com>
2020-01-06 17:36:53 +0300

    EDA started

--- jobs_04_data-wrangling.R
+++ jobs_04_data-wrangling.R
@@ -11,68 +11,71 @@ employers_wikidata <- filter(employers_wikidata, !garbage)
 saveRDS(employers_wikidata, 'data/employers_wikidata.RDS')
 summary(employers_wikidata)
 
-employers_wikidata <- employers_wikidata %>%
-  mutate_if(is.character, as.factor) %>%
-  mutate_at(dplyr::vars(employer.name, employer.id), as.character)
-glimpse(employers_wikidata)
-summary(employers_wikidata)
-unique(employers_wikidata$employer.country)
+# employers_wikidata <- employers_wikidata %>%
+#   mutate_if(is.character, as.factor) %>%
+#   mutate_at(dplyr::vars(employer.name, employer.id), as.character)
+# glimpse(employers_wikidata)
+# summary(employers_wikidata)
+# unique(employers_wikidata$employer.country)
+# 
+# c_russian <- c('Россия', 'Русское царство', 'Российская империя')
+# c_cis     <- c('Эстония', 'Украина', 'Казахстан')
+# c_other   <- c(
+#   'Китайская Народная Республика',
+#   'Турция',
+#   'Япония',
+#   'Египет',
+#   'Аргентина',
+#   'Катар'
+# )
+# employer_class_levels <- c(
+#   'Малое предприятие',
+#   'Среднее предприятие',
+#   'Крупное предприятие',
+#   'Корпорация'
+# )
+# 
+# hist(employers_wikidata$employer.no_employees)
+# 
+# wikidata <- employers_wikidata %>%
+#   mutate(
+#     employer.region = factor(
+#       case_when(
+#         is.na(employer.country) ~ 'Россия',
+#         employer.country %in% c_russian ~ 'Россия',
+#         employer.country %in% c_cis ~ 'быв. СССР',
+#         employer.country %in% c_other ~ 'Другой',
+#         TRUE ~ 'Запад'
+#       )
+#     ),
+#     employer.founded.cat = factor(
+#       case_when(
+#         is.na(employer.founded) ~ NA_character_,
+#         employer.founded >= 2010 ~ 'Z',
+#         between(employer.founded, 1989, 2009) ~ 'Y',
+#         between(employer.founded, 1949, 1988) ~ 'X',
+#         between(employer.founded, 1901, 1948) ~ 'Old',
+#         TRUE ~ 'Extremely old'
+#       ),
+#       ordered = TRUE
+#     ),
+#     employer.class = factor(
+#       case_when(
+#         employer.no_employees <= 50 ~ 'Малое предприятие',
+#         between(employer.no_employees, 51, 500) ~ 'Среднее предприятие',
+#         between(employer.no_employees, 501, 10000) ~ 'Крупное предприятие',
+#         is.na(employer.no_employees) ~ NA_character_,
+#         TRUE ~ 'Корпорация'
+#       ),
+#       ordered = TRUE,
+#       levels = employer_class_levels
+#     )
+#   ) %>%
+#   select(-c(employer.country, employer.founded, employer.headquarters, employer.no_employees))
+# glimpse(wikidata)
 
-c_russian <- c('Россия', 'Русское царство', 'Российская империя')
-c_cis     <- c('Эстония', 'Украина', 'Казахстан')
-c_other   <- c(
-  'Китайская Народная Республика',
-  'Турция',
-  'Япония',
-  'Египет',
-  'Аргентина',
-  'Катар'
-)
-employer_class_levels <- c(
-  'Малое предприятие',
-  'Среднее предприятие',
-  'Крупное предприятие',
-  'Корпорация'
-)
-
-hist(employers_wikidata$employer.no_employees)
-
-wikidata <- employers_wikidata %>%
-  mutate(
-    employer.region = factor(
-      case_when(
-        is.na(employer.country) ~ 'Россия',
-        employer.country %in% c_russian ~ 'Россия',
-        employer.country %in% c_cis ~ 'быв. СССР',
-        employer.country %in% c_other ~ 'Другой',
-        TRUE ~ 'Запад'
-      )
-    ),
-    employer.founded.cat = factor(
-      case_when(
-        is.na(employer.founded) ~ NA_character_,
-        employer.founded >= 2010 ~ 'Z',
-        between(employer.founded, 1989, 2009) ~ 'Y',
-        between(employer.founded, 1949, 1988) ~ 'X',
-        between(employer.founded, 1901, 1948) ~ 'Old',
-        TRUE ~ 'Extremely old'
-      ),
-      ordered = TRUE
-    ),
-    employer.class = factor(
-      case_when(
-        employer.no_employees <= 50 ~ 'Малое предприятие',
-        between(employer.no_employees, 51, 500) ~ 'Среднее предприятие',
-        between(employer.no_employees, 501, 10000) ~ 'Крупное предприятие',
-        is.na(employer.no_employees) ~ NA_character_,
-        TRUE ~ 'Корпорация'
-      ),
-      ordered = TRUE,
-      levels = employer_class_levels
-    )
-  ) %>%
-  select(-c(employer.country, employer.founded, employer.headquarters, employer.no_employees))
-glimpse(wikidata)
+wikidata <- mutate(employers_wikidata, employer.has_wiki = TRUE) %>%
+  select(employer.id, employer.has_wiki)
 
 saveRDS(wikidata, 'data/employers_wikidata.RDS')
 rm(
@@ -86,43 +89,47 @@ rm(employer_urls, employer_names, employer_ids, garbage, wikidata_get, wikidata_
 saveRDS(wikidata, 'data/employers_wikidata.RDS')
 
 ############ РАБОТОДАТЕЛИ ############
-employers <- left_join(employers, wikidata) %>%
-  mutate_if(is.character, as.factor) %>%
-  mutate_at(dplyr::vars(employer.name, employer.id), as.character) %>%
-  mutate_at(vars(contains('site')), as.character) %>%
-  mutate(
-    employer.region = as.character(employer.region),
-    employer.region = factor(
-      ifelse(
-        str_detect(employer.site_url, 'ru$'),
-        'Россия',
-        employer.region
-      )
-    )
-  )
+# employers <- left_join(employers, wikidata) %>%
+#   mutate_if(is.character, as.factor) %>%
+#   mutate_at(dplyr::vars(employer.name, employer.id), as.character) %>%
+#   mutate_at(vars(contains('site')), as.character) %>%
+#   mutate(
+#     employer.region = as.character(employer.region),
+#     employer.region = factor(
+#       ifelse(
+#         str_detect(employer.site_url, 'ru$'),
+#         'Россия',
+#         employer.region
+#       )
+#     )
+#   )
+# 
+# site_tmp <- stringdist(
+#   str_remove(employers$employer.site_url, '^https*\\://(www\\.)*'),
+#   str_remove(employers$employer.wiki_site, '^https*\\://(www\\.)*'),
+#   method = 'lv'
+# )
+# wikidata <- wikidata %>%
+#   anti_join(filter(employers, site_tmp >= 25))
+# employers <- readRDS('data/employers.RDS')
+# employers <- left_join(employers, wikidata) %>%
+#   mutate_if(is.character, as.factor) %>%
+#   mutate_at(dplyr::vars(employer.name, employer.id), as.character) %>%
+#   mutate_at(vars(contains('site')), as.character) %>%
+#   mutate(
+#     employer.region = as.character(employer.region),
+#     employer.region = factor(
+#       ifelse(
+#         str_detect(employer.site_url, 'ru$'),
+#         'Россия',
+#         employer.region
+#       )
+#     )
+#   )
 
-site_tmp <- stringdist(
-  str_remove(employers$employer.site_url, '^https*\\://(www\\.)*'),
-  str_remove(employers$employer.wiki_site, '^https*\\://(www\\.)*'),
-  method = 'lv'
-)
-wikidata <- wikidata %>%
-  anti_join(filter(employers, site_tmp >= 25))
-employers <- readRDS('data/employers.RDS')
-employers <- left_join(employers, wikidata) %>%
-  mutate_if(is.character, as.factor) %>%
-  mutate_at(dplyr::vars(employer.name, employer.id), as.character) %>%
-  mutate_at(vars(contains('site')), as.character) %>%
-  mutate(
-    employer.region = as.character(employer.region),
-    employer.region = factor(
-      ifelse(
-        str_detect(employer.site_url, 'ru$'),
-        'Россия',
-        employer.region
-      )
-    )
-  )
+employers <- employers %>%
+  left_join(wikidata) %>%
+  mutate(employer.has_wiki = if_else(is.na(employer.has_wiki), FALSE, TRUE))
 saveRDS(employers, 'data/employers.RDS')
 
 ############ ВАКАНСИИ ############
@@ -168,4 +175,35 @@ vacancies <- vacancies %>%
     ymd_hms
   )
 summary(vacancies)
+
+vacancies <- vacancies %>%
+  mutate(
+    description = if_else(
+      is.na(description_branded),
+      description,
+      description_branded
+    )
+  ) %>%
+  select(-description_branded) %>%
+  mutate(description = map_chr(description, strip_html))
 saveRDS(vacancies, 'data/vacancies.RDS')
+length(unique(vacancies$employer.id))
+
+df <- vacancies %>%
+  left_join(employers) %>%
+  mutate_at(
+    vars(
+      job,
+      site,
+      area,
+      address.metro.station,
+      address.metro.line,
+      department,
+      employer.type,
+      employer.area
+    ),
+    as.factor
+  ) %>%
+  select(-starts_with('salary.'))
+summary(df)
+saveRDS(df, 'data/headhunter.RDS')
---
+++ jobs_05_EDA.R
@@ -0,0 +1,1 @@
+df <- readRDS('data/headhunter.RDS')
