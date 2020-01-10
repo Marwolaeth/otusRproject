b7460fd63c4690134474e4cca551e1561edf2beb
Marwolaeth <a.pawluczenko@gmail.com>
2020-01-10 08:37:21 +0000

    Model evaluation for teachers

---
+++ jobs_09_проверка.R
@@ -0,0 +1,31 @@
+if (!require(pacman)) install.packages('pacman')
+pacman::p_load(smurf, dplyr, purrr)
+
+# Загрузка готовых моделей
+models_full <- readRDS('data/models/02_variables.RDS')
+
+# Метрики качества
+models_full$model_full %>%
+  map('accuracy') %>%
+  set_names(models_full$job) %>%
+  map(select, response, n, lambda, mean_abs_error, RMSE, R_sq.adj) %>%
+map(
+  set_names,
+  c(
+    'Зависимая переменная',
+    'Количество наблюдений',
+    'Вычисленная лямбда',
+    'Средняя абсолютная ошибка',
+    'Среднеквадратичная ошибка',
+    'R_sq.adj'
+  )
+)
+
+# Коэффициенты
+models_full$model_full %>%
+  map('coefficients') %>%
+  set_names(models_full$job) %>%
+  map(select, fname, ftype, beta) %>%
+  map(filter, beta != 0)
+
+# Объективно модели получились слабыми
